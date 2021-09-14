1c311284edaa9acf448c4d45aaa21638bfc17ead
PhilMackie <phil.mackie9@gmail.com>
2021-09-13 19:45:45 -0500

    done for the night

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function(str) {
+const reverseString = function(str){
+// splits it into a string
+const oldString = str.str('');
 
+
+
+// reverse the string and spits it out backwards due to string chaingunning
+
+
+
+
+//returns the VAR
+console.log(oldString);
 };
 
+reverseString();
 module.exports = reverseString;
